a680166f713dfa94ed68286dbef17e2a9dc40dbc
曾祥超 <zengxiangchao@do1.com.cn>
2020-12-01 17:50:24 +0800

    consumer增加k8s

--- consumer/src/main/java/cn/com/do1cloud/example/controller/IndexController.java
+++ consumer/src/main/java/cn/com/do1cloud/example/controller/IndexController.java
@@ -3,13 +3,10 @@ package cn.com.do1cloud.example.controller;
 import cn.com.do1cloud.example.api.EchoService;
 import com.alibaba.fastjson.JSONObject;
 import org.apache.dubbo.config.annotation.DubboReference;
-import org.apache.dubbo.rpc.RpcContext;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import static cn.com.do1cloud.example.api.Constants.TENANT_ID;
 
 /**
  * @author zengxc
@@ -23,7 +20,7 @@ public class IndexController {
 
     @RequestMapping(value = "/echo/{str}", method = RequestMethod.GET)
     public String echo(@PathVariable String str) {
-        RpcContext.getContext().setAttachment(TENANT_ID,"bb01a3cd987f469d9e0f04dffa2d3278");
+
             return JSONObject.toJSONString(echoService.echo(str));
     }
 
--- provider/src/main/java/cn/com/do1cloud/example/ProviderApplication.java
+++ provider/src/main/java/cn/com/do1cloud/example/ProviderApplication.java
@@ -1,6 +1,5 @@
 package cn.com.do1cloud.example;
 
-import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
