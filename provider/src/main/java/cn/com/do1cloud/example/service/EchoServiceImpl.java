65c169800253eb21f75f015f39a81923dfb9f3d7
曾祥超 <zengxiangchao@do1.com.cn>
2020-12-01 12:09:48 +0800

    修改om

--- provider/src/main/java/cn/com/do1cloud/example/service/EchoServiceImpl.java
+++ provider/src/main/java/cn/com/do1cloud/example/service/EchoServiceImpl.java
@@ -2,7 +2,6 @@ package cn.com.do1cloud.example.service;
 
 import cn.com.do1cloud.example.api.EchoService;
 import cn.com.do1cloud.example.api.FeignResult;
-import cn.com.do1cloud.example.api.RestException;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.apache.dubbo.rpc.RpcContext;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,10 +21,6 @@ public class EchoServiceImpl implements EchoService {
     @GetMapping("/echo")
     @Override
     public FeignResult echo(String message) {
-        RpcContext context = RpcContext.getContext();
-        if("error".equals(message)){
-            throw new RestException(1,"error msg","sssss");
-        }
         String token  = RpcContext.getContext().getAttachment(TENANT_ID);
         FeignResult result = new FeignResult();
         result.setData("["+token+"] Hello, " + message);
